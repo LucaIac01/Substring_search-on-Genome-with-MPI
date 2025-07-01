cfcf05b0133d12024d4a4fc1a7533c863bbb9c5a
LucaUni <luca.iacopino01@universitadipavia.it>
2025-07-01 04:28:14 +0200

    remove useless hash func and clear serial main

--- Serial/Advanced Computer Architecture/serial.c
+++ Serial/Advanced Computer Architecture/serial.c
@@ -1,54 +1,9 @@
-/////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-//////////////////////// Main Body \\\\\\\\\\\\\\\\\\\\\\\
-//////////////// Vito Giacalone  (546646) \\\\\\\\\\\\\\\\
-/////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
 #include <stdio.h>		
 #include <stdlib.h>
-#include "hashfun.h"
 #include <time.h>
 #include <stddef.h>
 
-///////////////////////////////Prototypes\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-char *readFile(char *filename, size_t *len);
 void rabin_karp2(char *txt, char *pattern, const size_t lentxt, const size_t lenpat, long long int *occurrences);
-
-///////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-//This is a function defined as a macro. This function has parametric
-//name, according to the hash function that is used.
-//The hashes of the pattern and of a portion of the text are computed
-//and iff the two hashes coincide, the check character by character 
-//is performed.
-///////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-#define RABIN_KARP_WITH_HASH(f) \
-long long int rabin_karp_##f(char *txt, char *pattern, const size_t lentxt, const size_t lenpat){\
-  long long int hashpat = f(pattern);\
-  char *chunk = (char*)malloc(sizeof(char)*(lenpat+1));\
-  long long int idxs = 0;\
-	for(size_t i = 0; i <= lentxt-lenpat; i++)\
-	{\
-		chunk = strncpy(chunk, txt+i, sizeof(char)*lenpat);\
-		chunk[lenpat]='\0';\
-		if (hashpat == f(chunk))\
-		{\
-			if (strcmp(pattern, chunk)==0)\
-			{\
-				idxs++;\
-			}\
-		}\
-	}\
-	return idxs;\
-  free(chunk);\
-}
-RABIN_KARP_WITH_HASH(polyHash)
-RABIN_KARP_WITH_HASH(djb2)
-RABIN_KARP_WITH_HASH(sdbm)
-RABIN_KARP_WITH_HASH(loselose)
-
-///////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-//This function computes the length of the file and read it.
-///////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 char *readFile(char *filename, size_t *len) {
     
@@ -71,12 +26,6 @@ char *readFile(char *filename, size_t *len) {
     return txt;
 }
 
-///////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-//The hashes of the pattern and of a portion of the text are computed
-//and iff the two hashes coincide, the check character by character 
-//is performed.
-//The total number of occurrences is returned
-///////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 void rabin_karp2(char *txt, char *pattern, const size_t lentxt, const size_t lenpat, long long int *occurrences){
 
@@ -144,9 +93,6 @@ void rabin_karp2(char *txt, char *pattern, const size_t lentxt, const size_t len
     }
 }
 
-///////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-/////////////////////////// Main function \\\\\\\\\\\\\\\\\\\\\\\\\\\\
-///////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 int main(int argc, char const *argv[])
 {
@@ -155,18 +101,15 @@ int main(int argc, char const *argv[])
 	size_t patlen;
 	if (argc != 3)
 	{
-		printf("Errore, inserire nome file\n");
+		printf("Error, insert file name\n");
 		exit(1);
 	}
 	long long int occurrences = 0;
 	char *pattern = readFile((char*)argv[2], &patlen);
 	char *txt = readFile((char*)argv[1], &txtlen);
-	//occurrences = rabin_karp_loselose(txt, pattern, txtlen, patlen);
-	//occurrences = rabin_karp_djb2(txt, pattern, txtlen, patlen);
-	//occurrences = rabin_karp_polyHash(txt, pattern, txtlen, patlen);
-	//occurrences = rabin_karp_sdbm(txt, pattern, txtlen, patlen);
+
 	rabin_karp2(txt, pattern, txtlen, patlen, &occurrences);
-	printf("OCCORRENZE TROVATE:%lld\n", occurrences);
+	printf("OCCURENCES FOUND:%lld\n", occurrences);
 
 
 	clock_t end = clock();
@@ -176,8 +119,3 @@ int main(int argc, char const *argv[])
 
 	return 0;
 }
-//END
-
-// http://www.cse.yorku.ca/~oz/hash.html funzioni di hashing semplici per stringhe
-// https://stackoverflow.com/questions/1579721/why-are-5381-and-33-so-important-in-the-djb2-algorithm
-//https://cp-algorithms.com/string/string-hashing.html	For simple (but not useful) hash function
